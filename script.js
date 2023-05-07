be303348ad25cc6d5b38c038a153e6755cd76972
Thays Martinez <thaysmartinez@gmail.com>
2023-05-07 17:48:26 +0200

    Create square grid

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const body = document.querySelector("body");
+const div = document.createElement("div");
+div.classList = "grid-container";
+
+body.appendChild(div);
+
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i = 0; i < 16 * 16; i++) {
+  const gridItem = document.createElement("div");
+  gridItem.classList = "grid-item";
+  gridContainer.appendChild(gridItem);
+}
