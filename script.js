5c658d97a64c00404fb09a4b79344be01e04cd9a
Thays Martinez <thaysmartinez@gmail.com>
2023-05-07 20:31:53 +0200

    Add logic to buttons

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 const body = document.querySelector("body");
-const div = document.createElement("div");
-div.classList = "grid-container";
+const divButtons = document.querySelectorAll(".btn-grid");
 
-body.appendChild(div);
+function generateGrid(cssClass, gridSize = 16) {
+  const gridTotal = gridSize * gridSize;
 
-const gridContainer = document.querySelector(".grid-container");
+  const div = document.createElement("div");
+  //   div.classList.remove("grid-container grid-16 grid-32 grid-64");
+  div.classList = `grid-container ${cssClass}`;
+  body.appendChild(div);
+  const gridContainer = document.querySelector(`.${cssClass}`);
 
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.classList = "grid-item";
-  gridContainer.appendChild(gridItem);
+  for (let i = 0; i < gridTotal; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList = "grid-item";
+    gridContainer.appendChild(gridItem);
+  }
 }
+
+function changeSize() {
+  divButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      switch (true) {
+        case button.getAttribute("id") === "grid-16":
+          generateGrid("grid-16", 16);
+          break;
+        case button.getAttribute("id") === "grid-32":
+          generateGrid("grid-32", 32);
+          break;
+        case button.getAttribute("id") === "grid-64":
+          generateGrid("grid-64", 64);
+          break;
+      }
+    });
+  });
+}
+
+generateGrid("grid-16");
+changeSize();
